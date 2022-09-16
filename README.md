e2f3f0f27b3355faa6798e62eb5d533c11d7f91a
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2022-09-16 09:27:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jc2yag
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jc2yag)
---
+++ index.js
@@ -0,0 +1,38 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+/*const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;*/
+
+//-------------------1----------------------
+/*
+const a = 10;
+const b = 6;
+
+console.log(a - b);
+console.log(a + b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+*/
+//------------------2-------------------
+const text = ` Typescript začal jako prototypovací scriptovací jazyk napsaný Brendanem Eichem v roce 1995 pro tehdejší Netscape browser pod názvem “Mocha”. `;
+
+const edit = text
+  .replaceAll(' ', '')
+  .toLowerCase()
+  .replaceAll('typescript', 'javascript');
+
+console.log(edit.startsWith('javascript'));
+console.log(edit);
+
+//----------------------3---------------------
+for (let i = 0; i < 8; i++) {
+  console.log(`Mám rád javascript po ${i}!`);
+}
+
+const name = 'Martin'.split('').reverse();
+for (let i = 0; i < 3; i++) {
+  console.log(name);
+}
