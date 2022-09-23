0734c1e5dc2abf62cbee2a57806563ca7e0cb964
Martin-Prusa <73097610+Martin-Prusa@users.noreply.github.com>
2022-09-23 08:48:11 +0200

    Hodina2

--- index.js
+++ index.js
@@ -36,3 +36,47 @@ const name = 'Martin'.split('').reverse().join('');
 for (let i = 0; i < 3; i++) {
   console.log(name);
 }
+
+//----------------------4---------------------
+const operace = '+';
+const a = 10;
+const b = 4;
+let c = 0;
+
+switch (operace) {
+  case '+':
+    c = a + b;
+    break;
+  case '-':
+    c = a - b;
+    break;
+  case '%':
+    c = a % b;
+    break;
+  case '*':
+    c = a * b;
+    break;
+  case '/':
+    c = a / b;
+    break;
+  default:
+    console.error('Error');
+    break;
+}
+
+console.log(a < 0 ? 'Záporné' : 'Kladné');
+
+//-------------------5----------------
+const arr = [3, 5, 7, 8, 11, 12, 13, 16, 17, 18]
+  .filter(i => i % 2 === 0)
+
+console.log(arr)
+
+//-------------------5----------------
+const myName = 'Martin Prusa'.split(' ')
+
+console.log(`Jmeno: ${myName[0]}`)
+console.log(`Prijmeni: ${myName[1]}`)
+console.log(`Inicialy: ${myName.map(i => i[0]).join('')}`)
+
+//------------------6----------------------
